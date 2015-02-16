3c9486a2bfde765d56693378b2201cbb32b24102
1002471 <1002471@SKP1002471MN001.local>
2015-02-16 11:32:40 +0900

    first commit

---
+++ Welicious.java
@@ -0,0 +1,21 @@
+class Member {
+	String id;
+	String name;
+	int age;
+	String[] interests;
+}
+
+class Group {
+	Location region;
+	String topic;
+	Member[] members;
+	Dining plan;
+}
+
+class Dining {
+	DateTime time;
+	String menu;
+	String place;
+}
+
+
