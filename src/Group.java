902d377366e26d8d3fdda06901ebf8e9d707a4f5
1ambda <1amb4a@gmail.com>
2015-02-16 13:16:40 +0900

    example 1

---
+++ src/Group.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+
+public class Group {
+
+	private String region;
+	private String topic;
+	private Dining dining;
+	private int maxMemberCount;
+	private ArrayList<Member> members;
+
+	public Group(String region, String topic, int maxMemberCount) {
+		
+		this.region = region;
+		this.topic = topic;
+		this.maxMemberCount = maxMemberCount;
+	}
+	
+	public int addMember(Member m) {
+		
+		if (members.size() < maxMemberCount) {
+			members.add(m);
+			// Successfully added
+			return 1;
+		}
+	
+		// Group is full
+		return -1;
+	}
+	
+	public void removeMemver(Member m) {
+		
+		for (Member t in members) {
+			if (t == m) {
+				members.
+			}
+		}
+	}
+}
